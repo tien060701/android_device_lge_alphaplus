8b41d26252537de38fad06e26708ff4e36dba1d1
freetime <43937423+BeanGTA@users.noreply.github.com>
2022-07-03 06:02:04 +0700

    Update and rename aosp_flashlmdd.mk to aosp_alphaplus.mk

--- aosp_alphaplus.mk
+++ aosp_alphaplus.mk
@@ -21,15 +21,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit from flashlmdd device
-$(call inherit-product, device/lge/flashlmdd/device.mk)
+$(call inherit-product, device/lge/alphaplus/device.mk)
 
 # Inherit some common AOSP stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifiers
 
-PRODUCT_NAME := aosp_flashlmdd
-PRODUCT_DEVICE := flashlmdd
+PRODUCT_NAME := aosp_alphaplus
+PRODUCT_DEVICE := alphaplus
 PRODUCT_BRAND := lge
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_RELEASE_NAME := V50 ThinQ
@@ -37,12 +37,12 @@ PRODUCT_MODEL := LM-V500
 
 PRODUCT_GMS_CLIENTID_BASE := android-lge
 
-TARGET_VENDOR_PRODUCT_NAME := flashlmdd_lao_com
-TARGET_VENDOR_DEVICE_NAME := flashlmdd
+TARGET_VENDOR_PRODUCT_NAME := alphaplus_lao_com
+TARGET_VENDOR_DEVICE_NAME := alphaplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="flashlmdd_lao_com" \
-    PRIVATE_BUILD_DESC="flashlmdd_lao_com-user 11 RKQ1.210420.001 2122112261c44 release-keys"
+    PRODUCT_NAME="alphaplus_lao_com" \
+    PRIVATE_BUILD_DESC="alphaplus_lao_com-user 11 RKQ1.210420.001 2122112261c44 release-keys"
 
-BUILD_FINGERPRINT := "lge/flashlmdd_lao_com/flashlmdd:11/RKQ1.210420.001/2122112261c44:user/release-keys"
+BUILD_FINGERPRINT := "lge/alphaplus_lao_com/alphaplus:11/RKQ1.210420.001/2122112261c44:user/release-keys"
 
